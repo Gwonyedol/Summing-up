0fc75df4e63018360c330159018d5c5b5adfda5e
mhgyj0916 <mhgyj0916@gmail.com>
2020-01-26 19:27:15 +0900

    Java Ex09 IO

--- Java/src/Day3.java
+++ Java/src/Day3.java
@@ -1,8 +1,15 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class Day3 {
   public static void main(String[] args) {
+    
+    /*
+    //collction framework 실습
     HashMap<String, Integer> test = new HashMap<String, Integer>();
     // put() 메소드를 이용한 요소의 저장
     test.put("일", 1);
@@ -23,5 +30,42 @@ public class Day3 {
     while(testKey.hasNext()) {
       System.out.println(test.get(testKey.next()));
     }
+    
+    try {
+      Exception e = new Exception("오류메시지");
+      throw e;
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    */
+    
+    
+    //IO 실습
+    
+    FileInputStream fs = null;
+    FileOutputStream fos = null;
+    
+    String path = "C:\\Temp\\a.txt";
+    
+    try {
+      fs= new FileInputStream(path);
+      fos = new FileOutputStream("C:\\Temp\\b.txt");
+      
+      int data = 0;
+      while((data=fs.read())!=-1) {
+        fos.write(data);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        fs.close();
+        fos.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }   
+    
+
   }
 }
