bd44032ba9969de82ab69d47fe12d15387a1e608
mhgyj0916 <mhgyj0916@gmail.com>
2020-01-30 21:29:10 +0900

    no message

--- JavaNote/src/Day8.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Day8 {
-	public static void main(String[] args) {
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		
-		System.out.println(a);
-	}
-}
